e58f9ccb04a614a88e4e913f9300a508afa3a7e4
Romulo Augusto <romulomail@gmail.com>
2023-09-20 01:08:31 -0300

    variável para o ano atual para a composição dos parâmetros

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 import { SicomDataDownloader, MeiosDeComunicacao } from "./modules/download/sicom-data-downloader.js";
 
 const downloader = new SicomDataDownloader();
+const anoAtual = new Date(Date.now()).getFullYear();
 
 let params = [];
-for (let ano = 2016; ano <= 2023; ano++) {
+for (let ano = 2016; ano <= anoAtual; ano++) {
     Object.values(MeiosDeComunicacao).forEach(async (meio) => {
         params.push({
             ano_acao: ano.toString(),
