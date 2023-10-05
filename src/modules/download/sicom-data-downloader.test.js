a2c495a2f2d36e691f97b2cc612e7ffaedb5c7c9
Romulo Augusto <romulomail@gmail.com>
2023-10-05 00:29:05 -0300

    #2 - habilita o vscode para usar es-module; configura o jest para usar es-module através do NODE_OPTIONS; cria primeiro teste válido

--- src/modules/download/sicom-data-downloader.test.js
+++ src/modules/download/sicom-data-downloader.test.js
@@ -1,3 +1,6 @@
-test("", () => {
-    expect(1 + 1).toBe(2);
+import { SicomDataDownloader } from "./sicom-data-downloader";
+
+it("deve ter charset padrão como iso-8859-1 ao instanciar", () => {
+    const downloader = new SicomDataDownloader();
+    expect(downloader.charset).toBe("iso-8859-1");
 });
